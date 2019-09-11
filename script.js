fac49f40d0a6bcb060be915b61a10c0c819b2055
Andrei Marius Hutupas Antoniu <andreimantoniu@yahoo.com>
2019-09-11 23:42:23 +0200

    11C_03_02_v1

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let inputElmt = document.querySelector("#color-selector");
+let input = document.querySelector("#color-selector");
+const container = document.querySelectorAll("#container>div");
+const select = document.querySelector("select");
+let option = document.querySelector("option");
+let harmony = "analogous";
 
-inputElmt.addEventListener("input", function showValues() {
-    document.querySelector(".hex-value").innerHTML = inputElmt.value;
-    document.querySelector(".rgb-value").innerHTML = convertHEXtoRGB(inputElmt.value);
-    document.querySelector(".hex-value").style.color = inputElmt.value;
-    document.querySelector(".rgb-value").style.color = inputElmt.value;
-}, false);
+input.addEventListener("input", getColors, false);
 
+function getColors() {
+    convertHEXtoRGB(input.value);
+}
+
+select.onchange = function () {
+    harmony = select.value;
+    convertHEXtoRGB(input.value);
+}
 
 function convertHEXtoRGB(h) {
     let r = 0,
@@ -18,8 +25,9 @@ function convertHEXtoRGB(h) {
     b = "0x" + h[5] + h[6];
 
     convertRGBtoHSL(r, g, b);
-    return +r + ", " + +g + ", " + +b;
+    return +r + "," + +g + "," + +b;
 }
+
 
 function convertRGBtoHSL(r, g, b) {
 
@@ -37,18 +45,14 @@ function convertRGBtoHSL(r, g, b) {
     if (delta == 0)
         h = 0;
     else
-
     if (max == r)
         h = ((g - b) / delta) % 6;
     else
-
     if (max == g)
         h = (b - r) / delta + 2;
     else
         h = (r - g) / delta + 4;
-
     h = Math.round(h * 60);
-
     if (h < 0)
         h += 360;
 
@@ -59,6 +63,68 @@ function convertRGBtoHSL(r, g, b) {
     s = +(s * 100).toFixed(1);
     l = +(l * 100).toFixed(1);
 
-    document.querySelector(".hsl-value").innerHTML = h + ", " + s + "%, " + l + "%";
-    document.querySelector(".hsl-value").style.color = inputElmt.value;
+    calcHarmony(h, s, l);
+}
+
+function calcHarmony(h, s, l) {
+    hBase = h;
+    sBase = s;
+    lBase = l;
+    let x = 0;
+
+    for (i = -2; i < 3; i++) {
+        if (harmony == "analogous") {
+            h = hBase + (20 * i);
+        }
+        if (harmony == "monochromatic") {
+            l = (lBase + 15 * i).toFixed(1);
+        }
+        if (harmony == "triad") {
+            h = hBase + (30 * i);
+        }
+        if (harmony == "complementary") {
+            h = hBase + (60 * i);
+        }
+        if (harmony == "compound") {
+            if (i < 0) {
+                h = hBase + (60 * i);
+            }
+            if (i > 0) {
+                h = hBase + (20 * i);
+            }
+        }
+        if (harmony == "shades") {
+            s = (lBase + 20 * i).toFixed(1);
+        }
+
+        container[x].style.backgroundColor = `hsl(${h},${s}%,${l}%)`;
+        container[2].style.backgroundColor = `hsl(${hBase},${sBase}%,${lBase}%)`;
+        container[x].firstElementChild.lastElementChild.innerHTML = `${h},${s}%,${l}%`;
+        let rgb = container[x].style.backgroundColor;
+
+        function setRGB(rgb) {
+            let sep = rgb.indexOf(", ") > -1 ? ", " : " ";
+            rgb = rgb.substr(4).split(")")[0].split(sep);
+            return rgb;
+        }
+
+        function convertRGBtoHex(rgb) {
+            let r = (+rgb[0]).toString(16),
+                g = (+rgb[1]).toString(16),
+                b = (+rgb[2]).toString(16);
+
+            if (r.length == 1)
+                r = "0" + r;
+            if (g.length == 1)
+                g = "0" + g;
+            if (b.length == 1)
+                b = "0" + b;
+
+            return "#" + r + g + b;
+        }
+
+        container[x].firstElementChild.children[3].innerHTML = setRGB(rgb);
+        container[x].firstElementChild.children[1].innerHTML = convertRGBtoHex(setRGB(rgb));
+        x++;
+    }
 }
